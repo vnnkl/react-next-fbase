b5208ae7e6fbdc32bcdd66f1e15420a17dac5e15
Constantin <CVennekel@gmail.com>
2021-04-18 21:26:11 +0200

    add paths/styles/Loader

---
+++ pages/[username]/index.js
@@ -0,0 +1,7 @@
+export default function UsernameProfilePage({ }) {
+  return (
+    <main>
+        
+    </main>
+  )
+}
---
+++ pages/admin/[slug].js
@@ -0,0 +1,7 @@
+export default function AdminPostsEditPage({ }) {
+  return (
+    <main>
+        <h1>Edit Post</h1>
+    </main>
+  )
+}
