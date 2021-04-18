9da0927b8f6c8da5b639665046c4f9e8d52d1955
Constantin <CVennekel@gmail.com>
2021-04-18 23:06:04 +0200

    add toast

---
+++ components/NavBar.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link';
+
+export default function NavBar(){
+ const user = true;
+ const username = true;
+
+    return (
+        <nav className="navbar"> 
+            <ul>
+                <li>
+                    <Link href="/">
+                        <button className={"btn-logo"}>FEED</button>
+                    </Link>
+                </li>
+
+                {/* User is signed-in and has username*/}
+                {username && (
+                    <>
+                    <li className={"push-left"}>
+                    <Link href={"/admin"}>
+                        <button className={"btn-blue"}>Write Posts</button>
+                    </Link>
+                    </li>
+                    <li> 
+                    <Link href={`/${username}`}>
+                        <img src={user?.photoURL} />
+                    </Link>
+                    </li>
+                    </>
+                )}
+
+                {/* User is NOT signed-in or has not created username*/}
+                {!username && (
+                    <li>
+                        <Link href="/enter">
+                            <button className={"btn-blue"}>Log in </button>
+                        </Link>
+                    </li>
+                    )}
+
+            </ul>
+        </nav>
+    )
+}
--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,7 @@
 export default function UsernameProfilePage({ }) {
   return (
     <main>
-        
+      <h1>User Profile</h1>
     </main>
   )
 }
