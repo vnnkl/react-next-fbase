c9b8c7b79d0e3508068fab900ae0415d7ac2a671
Constantin <CVennekel@gmail.com>
2021-04-22 13:57:48 +0200

    dynamic pages

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,35 @@
-export default function UsernameProfilePage({ }) {
+import PostFeed from '../../components/PostFeed'
+import UserProfile from '../../components/UserProfile'
+import {getUserWithUsername, postToJSON} from "../../lib/firebase";
+
+
+export async function getServerSideProps({query}){
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc){
+    user = userDoc.data();
+    const postQuery = userDoc.ref.collection('posts').where('published','==',true).orderBy('createdAt','desc').limit(5);
+
+    posts = (await postQuery.get()).docs.map(postToJSON)
+  }
+
+  return {
+    props: {user, posts} // will be passed to component page as props
+  }
+}
+
+export default function UsernameProfilePage({ user, posts }) {
   return (
     <main>
       <h1>User Profile</h1>
+        <UserProfile user={user}/>
+        <PostFeed posts={posts}/>
     </main>
   )
 }
