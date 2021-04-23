a211a2be82cedabeeef04787b6c8d42d06fed91f
Constantin <CVennekel@gmail.com>
2021-04-23 15:53:34 +0200

    Add Metatags

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,6 +1,9 @@
+import MetaTags from "../../components/Metatags";
+
 export default function AdminPostsEditPage({ }) {
   return (
     <main>
+        <MetaTags title={'admin page'} />
         <h1>Edit Post</h1>
     </main>
   )
