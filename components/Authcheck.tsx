4410b2252d4600b8efab4db5656f3b229892fcae
Constantin <CVennekel@gmail.com>
2021-04-24 12:18:52 +0200

    fix

--- components/Authcheck.tsx
+++ components/Authcheck.tsx
@@ -1,10 +1,9 @@
 import Link from 'next/link';
 import {useContext} from "react";
 import {UserContext} from '../lib/context';
-import {AppPropsType} from "next/dist/next-server/lib/utils";
 
 // Components children only shown to authenticated users
-export default function AuthCheck(props) {
+export default function AuthCheck(props: { children: any; fallback: any; }) {
     const { username } = useContext(UserContext);
 
     return username ? props.children : props.fallback || <Link href={'/enter'}>You must be signed in</Link>;
